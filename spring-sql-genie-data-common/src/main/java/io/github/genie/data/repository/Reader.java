5948cd5312cfaf41a8659ff6d3675ea782413eec
huangchengwei <huang.chengwei@outlook.com>
2024-01-23 15:16:05 +0800

    build: update version to `1.1.0-SNAPSHOT`

--- spring-sql-genie-data-common/src/main/java/io/github/genie/data/repository/Reader.java
+++ spring-sql-genie-data-common/src/main/java/io/github/genie/data/repository/Reader.java
@@ -1,10 +1,10 @@
 package io.github.genie.data.repository;
 
 import io.github.genie.sql.api.ExpressionHolder;
+import io.github.genie.sql.api.ExpressionHolder.ColumnHolder;
 import io.github.genie.sql.api.ExpressionOperator.ComparableOperator;
 import io.github.genie.sql.api.ExpressionOperator.NumberOperator;
 import io.github.genie.sql.api.ExpressionOperator.PathOperator;
-import io.github.genie.sql.api.ExpressionOperator.Predicate;
 import io.github.genie.sql.api.ExpressionOperator.StringOperator;
 import io.github.genie.sql.api.LockModeType;
 import io.github.genie.sql.api.Order;
@@ -15,38 +15,22 @@ import io.github.genie.sql.api.Path.NumberPath;
 import io.github.genie.sql.api.Path.StringPath;
 import io.github.genie.sql.api.Query.AndBuilder;
 import io.github.genie.sql.api.Query.Collector;
+import io.github.genie.sql.api.Query.OrderBy;
+import io.github.genie.sql.api.Query.OrderOperator;
 import io.github.genie.sql.api.Query.Select;
 import io.github.genie.sql.api.Query.Where;
 import io.github.genie.sql.api.Query.Where0;
 import io.github.genie.sql.api.Slice;
 import io.github.genie.sql.api.Sliceable;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.function.Function;
 
 interface Reader<T> {
     Select<T> query();
-
-    default Where<T, T> fetch(List<PathOperator<T, ?, Predicate<T>>> expressions) {
-        return query().fetch(expressions);
-    }
-
-    default Where<T, T> fetch(Collection<Path<T, ?>> paths) {
-        return query().fetch(paths);
-    }
-
-    default Where<T, T> fetch(Path<T, ?> path) {
-        return query().fetch(path);
-    }
-
-    default Where<T, T> fetch(Path<T, ?> p0, Path<T, ?> p1) {
-        return query().fetch(p0, p1);
-    }
-
-    default Where<T, T> fetch(Path<T, ?> p0, Path<T, ?> p1, Path<T, ?> p3) {
-        return query().fetch(p0, p1, p3);
-    }
 
     default <R> Where<T, R> select(Class<R> projectionType) {
         return query().select(projectionType);
@@ -60,8 +44,8 @@ interface Reader<T> {
         return query().select(expression);
     }
 
-    default <R> Where0<T, R> select(Path<T, ? extends R> expression) {
-        return query().select(expression);
+    default <R> Where0<T, R> select(Path<T, ? extends R> path) {
+        return query().select(path);
     }
 
     default Where0<T, Object[]> select(Collection<Path<T, ?>> paths) {
@@ -104,6 +88,104 @@ interface Reader<T> {
         return query().select(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9);
     }
 
+    default <R> Where<T, R> selectDistinct(Class<R> projectionType) {
+        return query().selectDistinct(projectionType);
+    }
+
+    default Where0<T, Object[]> selectDistinct(List<? extends ExpressionHolder<T, ?>> paths) {
+        return query().selectDistinct(paths);
+    }
+
+    default <R> Where0<T, R> selectDistinct(ExpressionHolder<T, R> expression) {
+        return query().selectDistinct(expression);
+    }
+
+    default <R> Where0<T, R> selectDistinct(Path<T, ? extends R> path) {
+        return query().selectDistinct(path);
+    }
+
+    default Where0<T, Object[]> selectDistinct(Collection<Path<T, ?>> paths) {
+        return query().selectDistinct(paths);
+    }
+
+    default Where0<T, Object[]> selectDistinct(Path<T, ?> p0, Path<T, ?> p1) {
+        return query().selectDistinct(p0, p1);
+    }
+
+    default Where0<T, Object[]> selectDistinct(Path<T, ?> p0, Path<T, ?> p1, Path<T, ?> p2) {
+        return query().selectDistinct(p0, p1, p2);
+    }
+
+    default Where0<T, Object[]> selectDistinct(Path<T, ?> p0, Path<T, ?> p1, Path<T, ?> p2, Path<T, ?> p3) {
+        return query().selectDistinct(p0, p1, p2, p3);
+    }
+
+    default Where0<T, Object[]> selectDistinct(Path<T, ?> p0, Path<T, ?> p1, Path<T, ?> p2, Path<T, ?> p3, Path<T, ?> p4) {
+        return query().selectDistinct(p0, p1, p2, p3, p4);
+    }
+
+    default Where0<T, Object[]> selectDistinct(Path<T, ?> p0, Path<T, ?> p1, Path<T, ?> p2, Path<T, ?> p3,
+                                               Path<T, ?> p4, Path<T, ?> p5) {
+        return query().selectDistinct(p0, p1, p2, p3, p4, p5);
+    }
+
+    default Where0<T, Object[]> selectDistinct(Path<T, ?> p0, Path<T, ?> p1, Path<T, ?> p2, Path<T, ?> p3,
+                                               Path<T, ?> p4, Path<T, ?> p5, Path<T, ?> p6) {
+        return query().selectDistinct(p0, p1, p2, p3, p4, p5, p6);
+    }
+
+    default Where0<T, Object[]> selectDistinct(Path<T, ?> p0, Path<T, ?> p1, Path<T, ?> p2, Path<T, ?> p3,
+                                               Path<T, ?> p4, Path<T, ?> p5, Path<T, ?> p6, Path<T, ?> p7) {
+        return query().selectDistinct(p0, p1, p2, p3, p4, p5, p6, p7);
+    }
+
+    default Where0<T, Object[]> selectDistinct(Path<T, ?> p0, Path<T, ?> p1, Path<T, ?> p2, Path<T, ?> p3,
+                                               Path<T, ?> p4, Path<T, ?> p5, Path<T, ?> p6, Path<T, ?> p7, Path<T, ?> p8) {
+        return query().selectDistinct(p0, p1, p2, p3, p4, p5, p6, p7, p8);
+    }
+
+    default Where0<T, Object[]> selectDistinct(Path<T, ?> p0, Path<T, ?> p1, Path<T, ?> p2, Path<T, ?> p3,
+                                               Path<T, ?> p4, Path<T, ?> p5, Path<T, ?> p6, Path<T, ?> p7,
+                                               Path<T, ?> p8, Path<T, ?> p9) {
+        return query().selectDistinct(p0, p1, p2, p3, p4, p5, p6, p7, p8, p9);
+    }
+
+    default Where<T, T> fetch(List<ColumnHolder<T, ?>> expressions) {
+        return query().fetch(expressions);
+    }
+
+    default Where<T, T> fetch(ColumnHolder<T, ?> path) {
+        return query().fetch(path);
+    }
+
+    default Where<T, T> fetch(ColumnHolder<T, ?> p0, ColumnHolder<T, ?> p1) {
+        return query().fetch(p0, p1);
+    }
+
+    default Where<T, T> fetch(ColumnHolder<T, ?> p0, ColumnHolder<T, ?> p1, ColumnHolder<T, ?> p3) {
+        return query().fetch(p0, p1, p3);
+    }
+
+    default Where<T, T> fetch(Collection<Path<T, ?>> paths) {
+        return query().fetch(paths);
+    }
+
+    default Where<T, T> fetch(Path<T, ?> path) {
+        return query().fetch(path);
+    }
+
+    default Where<T, T> fetch(Path<T, ?> p0, Path<T, ?> p1) {
+        return query().fetch(p0, p1);
+    }
+
+    default Where<T, T> fetch(Path<T, ?> p0, Path<T, ?> p1, Path<T, ?> p3) {
+        return query().fetch(p0, p1, p3);
+    }
+
+    default OrderBy<T, T> where(ExpressionHolder<T, Boolean> predicate) {
+        return query().where(predicate);
+    }
+
     default <N> PathOperator<T, N, ? extends AndBuilder<T, T>> where(Path<T, N> path) {
         return query().where(path);
     }
@@ -124,23 +206,39 @@ interface Reader<T> {
         return query().where(path);
     }
 
-    default Collector<T> orderBy(List<? extends Order<T>> path) {
-        return query().orderBy(path);
+    default Collector<T> orderBy(List<? extends Order<T>> orders) {
+        return query().orderBy(orders);
     }
 
-    default Collector<T> orderBy(Order<T> path) {
-        return query().orderBy(path);
+    default Collector<T> orderBy(Order<T> order) {
+        return query().orderBy(order);
     }
 
     default Collector<T> orderBy(Order<T> p0, Order<T> p1) {
         return query().orderBy(p0, p1);
     }
 
-    default Collector<T> orderBy(Order<T> p0, Order<T> p1, Order<T> p2) {
-        return query().orderBy(p0, p1, p2);
+    default Collector<T> orderBy(Order<T> order1, Order<T> order2, Order<T> order3) {
+        return query().orderBy(order1, order2, order3);
     }
 
-    default int count() {
+    default OrderOperator<T, T> orderBy(Collection<Path<T, Comparable<?>>> paths) {
+        return query().orderBy(paths);
+    }
+
+    default OrderOperator<T, T> orderBy(Path<T, Comparable<?>> path) {
+        return query().orderBy(path);
+    }
+
+    default OrderOperator<T, T> orderBy(Path<T, Comparable<?>> p1, Path<T, Comparable<?>> p2) {
+        return query().orderBy(p1, p2);
+    }
+
+    default OrderOperator<T, T> orderBy(Path<T, Comparable<?>> p1, Path<T, Comparable<?>> p2, Path<T, Comparable<?>> p3) {
+        return query().orderBy(p1, p2, p3);
+    }
+
+    default long count() {
         return query().count();
     }
 
@@ -190,10 +288,6 @@ interface Reader<T> {
 
     default T getSingle(int offset) {
         return query().getSingle(offset);
-    }
-
-    default List<T> getList(int offset) {
-        return query().getList(offset);
     }
 
     default List<T> getList() {
@@ -246,6 +340,10 @@ interface Reader<T> {
 
     default List<T> getList(LockModeType lockModeType) {
         return query().getList(lockModeType);
+    }
+
+    default <R> R getResult(@NotNull Function<? super Collector<T>, R> function) {
+        return query().getResult(function);
     }
 
     default <R> R slice(Sliceable<T, R> sliceable) {
